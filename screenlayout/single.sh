c3fe55b7140fa9afa4941a1b68d1f97298751d81
Jaseem Abid <jaseemabid@gmail.com>
2014-01-26 15:53:02 +0530

    Remove screenlayout files

--- screenlayout/single.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-0 --off --output LVDS --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-0 --off
--- screenlayout/triple.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-0 --mode 1280x1024 --pos 0x0 --rotate normal --output LVDS --mode 1366x768 --pos 1280x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 2646x0 --rotate normal
