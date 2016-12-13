1815ff88f6c3131a23cf21fc754645fe9de699fb
Jaseem Abid <jaseem@instamojo.com>
2016-12-13 09:10:12 +0530

    Fix brew setup script

--- brew/setup.sh
+++ brew/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 echo "Setup brew"
 
 brew update
@@ -8,59 +10,28 @@ brew tap railwaycat/emacsmacport
 brew cask install emacs-mac
 brew cask install font-fira-code
 
-brew install aspell \
-             autoconf \
-             diff-so-fancy \
-             dirmngr \
-             doxygen \
-             erlang \
-             ffmpeg \
-             gdbm \
-             git \
-             gnupg2 \
-             gpg-agent \
-             haskell-stack \
-             htop \
-             jpeg \
-             lame \
-             libassuan \
-             libevent \
-             libgcrypt \
-             libgpg-error \
-             libksba \
-             libpng \
-             libtiff \
-             libusb \
-             libusb-compat \
-             md5sha1sum \
-             memcached \
-             mtr \
-             mutt \
-             openssl \
-             pcre \
-             pinentry \
-             pinentry-mac \
-             pkg-config \
-             pth \
-             python \
-             python3 \
-             ranger \
-             readline \
-             sloccount \
-             sqlite \
-             stow \
-             thefuck \
-             tig \
-             tmux \
-             tokyo-cabinet \
-             unixodbc \
-             utf8proc \
-             wget \
-             wxmac \
-             x264 \
-             xvid \
-             xz \
-             youtube-dl \
-             zsh
+brew install aspell
+brew install automake     # Tool for generating GNU compliant Makefiles
+brew install coreutils    # File, shell and text manipulation utilities
+brew install diff-so-fancy
+brew install erlang
+brew install ffmpeg
+brew install git
+brew install gnupg2
+brew install haskell-stack
+brew install highlight
+brew install htop
+brew install mtr
+brew install pinentry-mac # So that emacs can ask for pass phrase on mac
+brew install python
+brew install ranger
+brew install reattach-to-user-namespace
+brew install stow
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install wget
+brew install youtube-dl
+brew install zsh
 
 brew linkapps
