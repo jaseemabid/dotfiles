ff2557df6842e11f1e3cc4412f167f8d199c9cd5
Jaseem Abid <jaseemabid@gmail.com>
2017-05-24 00:19:34 -0400

    Setup icon fonts for xmobar

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -1,14 +1,14 @@
 Config {
-    font = "xft:Fira Code Mono:size=16:antialias=true"
+    font = "xft:Fira Code Mono:size=16,FontAwesome:size=15:antialias=true"
   , bgColor = "#002b36"
   , fgColor = "#657b83"
   , position = Bottom x
   , sepChar = "%"
   , alignSep = "}{"
-  , template = " %StdinReader% }{ %memory% | %multicpu% | %coretemp% | %dynnetwork% | %battery% | <fc=#ee9a00>%date%</fc>  "
+  , template = " %StdinReader% }{ %multicpu%    %memory%   %coretemp%   %dynnetwork%  %battery%  %date% "
   , commands = [
     -- battery monitor
-    Run Battery      [ "--template" , "Batt: <acstatus>"
+    Run Battery      [ "--template" , " <acstatus>"
                      , "--Low"      , "10"        -- units: %
                      , "--High"     , "80"        -- units: %
                      , "--low"      , "darkred"
@@ -25,7 +25,7 @@ Config {
                      ] 50
 
     -- network activity monitor (dynamic interface resolution)
-    , Run DynNetwork [ "--template" , "Net: [<tx>kB/s <rx>kB/s]"
+    , Run DynNetwork [ "--template" , " [<tx>kB/s <rx>kB/s]"
                      , "--Low"      , "1000"       -- units: B/s
                      , "--High"     , "5000"       -- units: B/s
                      , "--low"      , "darkgreen"
@@ -34,7 +34,7 @@ Config {
                      ] 10
 
     -- cpu activity monitor
-    , Run MultiCpu   [ "--template" , "CPU: [<total0>, <total1>, <total2>, <total3>]%"
+    , Run MultiCpu   [ "--template" , " [<total0>, <total1>, <total2>, <total3>]%"
                      , "--Low"      , "50"         -- units: %
                      , "--High"     , "85"         -- units: %
                      , "--low"      , "darkgreen"
@@ -43,7 +43,7 @@ Config {
                      ] 10
 
       -- cpu core temperature monitor
-    , Run CoreTemp   [ "--template" , "Temp: [<core0>°C <core1>°C]"
+    , Run CoreTemp   [ "--template" , " [<core0>°C <core1>°C]"
                      , "--Low"      , "70"        -- units: °C
                      , "--High"     , "80"        -- units: °C
                      , "--low"      , "darkgreen"
@@ -52,7 +52,7 @@ Config {
                      ] 50
 
     -- memory usage monitor
-    , Run Memory     [ "--template" ,"Mem: <usedratio>%"
+    , Run Memory     [ "--template" ," <usedratio>%"
                      , "--Low"      , "20"        -- units: %
                      , "--High"     , "90"        -- units: %
                      , "--low"      , "darkgreen"
@@ -61,6 +61,6 @@ Config {
                      ] 10
 
     -- , Run Com "~/.xmonad/get-volume" [] "myvolume" 10
-    , Run Date "%a %b %d%l:%M:%S %p" "date" 10
+    , Run Date " %a %b %d    %l:%M:%S %p " "date" 10
     , Run StdinReader]
 }
