0d670fd716ebdeaa535438c8e76f82334a256c05
Jaseem Abid <jaseemabid@gmail.com>
2017-06-01 11:39:04 -0400

    Cleanup xmobar
    - Add volume, brightness
    - Fix icons
    - Remove second temp

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -4,11 +4,16 @@ Config {
   , fgColor = "#657b83"
   , position = Bottom x
   , sepChar = "%"
+  , allDesktops = True
   , alignSep = "}{"
-  , template = " %StdinReader% }{ %multicpu%   %memory%   %coretemp%   %dynnetwork%   %battery%   %date% "
+  , template = " %StdinReader% }{ %dynnetwork%   %multicpu%   %memory%   %bright%   %volume%   %coretemp%   %battery%   %date% "
   , commands = [
+    Run Brightness   ["--template"  , " <percent>%"
+                     , "--"
+                     , "--device"   ,  "intel_backlight"] 100
+
     -- battery monitor
-    Run Battery      [ "--template" , " <acstatus>"
+  , Run Battery      [ "--template" , " <acstatus>"
                      , "--Low"      , "10"        -- units: %
                      , "--High"     , "80"        -- units: %
                      , "--low"      , "darkred"
@@ -19,13 +24,13 @@ Config {
                      -- discharging status
                      , "-o"         ,"<left>%"
                      -- AC "on" status
-                     , "-O"	    , "<fc=#dAA520>Charging</fc>"
+                     , "-O"	    , "<fc=#dAA520></fc>"
                      -- charged status
-                     , "-i"	    , "<fc=#006000>Charged</fc>"
+                     , "-i"	    , "<fc=#006000></fc>"
                      ] 50
 
     -- network activity monitor (dynamic interface resolution)
-    , Run DynNetwork [ "--template" , " [<tx>kB/s <rx>kB/s]"
+    , Run DynNetwork [ "--template" , " [<rx> <tx>] kB/s"
                      , "--Low"      , "1000"       -- units: B/s
                      , "--High"     , "5000"       -- units: B/s
                      , "--low"      , "darkgreen"
@@ -34,22 +39,23 @@ Config {
                      ] 10
 
     -- cpu activity monitor
-    , Run MultiCpu   [ "--template" , " [<total0> <total1> <total2> <total3>]%"
+    , Run MultiCpu   [ "--template" , " [<autototal>]%"
                      , "--Low"      , "50"         -- units: %
                      , "--High"     , "85"         -- units: %
                      , "--low"      , "darkgreen"
                      , "--normal"   , "darkorange"
                      , "--high"     , "darkred"
+                     , "-p"         , "2"
                      ] 10
 
       -- cpu core temperature monitor
-    , Run CoreTemp   [ "--template" , " [<core0>°C <core1>°C]"
+    , Run CoreTemp   [ "--template" , " <core0>°C"
                      , "--Low"      , "70"        -- units: °C
                      , "--High"     , "80"        -- units: °C
                      , "--low"      , "darkgreen"
                      , "--normal"   , "darkorange"
                      , "--high"     , "darkred"
-                     ] 50
+                     ] 100
 
     -- memory usage monitor
     , Run Memory     [ "--template" ," <usedratio>%"
@@ -60,7 +66,7 @@ Config {
                      , "--high"     , "darkred"
                      ] 10
 
-    -- , Run Com "~/.xmonad/get-volume" [] "myvolume" 10
+    , Run Com "volume" [] "" 10
     , Run Date " %a %b %d   %l:%M:%S %p " "date" 10
     , Run StdinReader]
 }
