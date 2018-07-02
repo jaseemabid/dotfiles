07b963e836a45b749b941f73e7ca448ea121e1ae
Jaseem Abid <jaseemabid@gmail.com>
2018-07-02 13:36:48 +0530

    Make arch bootstrap script idempotent as much as possible

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -1,5 +1,16 @@
-sudo pacman -S $(cat packages.txt)
+#!/bin/env bash
 
-cd /tmp && git clone https://aur.archlinux.org/yaourt.git
-cd /tmp/yaourt && makepkg -si
+if [ -x "$(which yaourt)" ]; then
+    echo "Yaourt installed already"
+else
+    echo "Installing yaourt"
 
+    cd /tmp && git clone https://aur.archlinux.org/yaourt.git
+    cd /tmp/yaourt && makepkg -si
+fi
+
+echo "Installing $(wc -l < packages.txt) packages from core"
+sudo pacman -S "$(cat packages.txt)"
+
+echo "Installing $(wc -l < aur.txt) packages from aur"
+yaourt --noconfirm -S "$(cat aur.txt)"
