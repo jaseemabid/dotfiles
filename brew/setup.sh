c9fe8d91cdbf657e511bc3e78e822160b96ec1ea
Jaseem Abid <jaseemabid@gmail.com>
2018-08-20 15:55:43 +0100

    Install a few more apps with brew

--- brew/setup.sh
+++ brew/setup.sh
@@ -6,14 +6,17 @@ brew update
 
 brew tap caskroom/fonts
 brew tap railwaycat/emacsmacport
+brew tap homebrew/cask-drivers
 
 brew cask install emacs-mac
+brew cask install iterm2
+brew cask install yubico-yubikey-manager
 
 brew install aspell
 brew install automake     # Tool for generating GNU compliant Makefiles
+brew install cdiff        # For side by side diffs, use with a -s
 brew install coreutils    # File, shell and text manipulation utilities
 brew install cowsay
-brew install cdiff        # For side by side diffs, use with a -s
 brew install diff-so-fancy
 brew install erlang
 brew install ffmpeg
@@ -24,6 +27,7 @@ brew install highlight
 brew install htop
 brew install jq
 brew install mtr
+brew install nvim
 brew install pinentry-mac # So that emacs can ask for pass phrase on mac
 brew install python
 brew install ranger
@@ -32,8 +36,10 @@ brew install stow
 brew install the_silver_searcher
 brew install tig
 brew install tmux
+brew install trash-cli
 brew install wget
 brew install youtube-dl
+brew install z
 brew install zsh
 
 brew upgrade
