4be1d274e59cc034e9186f0b5203a96ae35637d9
Jaseem Abid <jaseemabid@monzo.com>
2018-11-11 01:33:32 +0000

    Fix arch bootstrap script

--- arch/bootstrap.sh
+++ arch/bootstrap.sh
@@ -1,16 +1,17 @@
 #!/bin/env bash
 
+echo "Installing $(wc -l < packages.txt) packages from core"
+sudo pacman --noconfirm -S - < packages.txt
+
 if [ -x "$(which yaourt)" ]; then
     echo "Yaourt installed already"
 else
     echo "Installing yaourt"
 
+    rm -rf /tmp/yaourt
     cd /tmp && git clone https://aur.archlinux.org/yaourt.git
     cd /tmp/yaourt && makepkg -si
 fi
 
-echo "Installing $(wc -l < packages.txt) packages from core"
-sudo pacman -S "$(cat packages.txt)"
-
 echo "Installing $(wc -l < aur.txt) packages from aur"
-yaourt --noconfirm -S "$(cat aur.txt)"
+yaourt --noconfirm -S - < aur.txt
