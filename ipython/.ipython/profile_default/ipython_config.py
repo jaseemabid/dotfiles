a5893dd1f294c91992f1cd8f294ead7bcba72b52
Jaseem Abid <me@jabid.in>
2023-01-02 20:03:51 +0000

    Auto reload in ipython by default

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -35,6 +35,9 @@ c = get_config()  #noqa
 ## lines of code to run at IPython startup.
 #  Default: []
 # c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 
 ## A list of dotted module names of IPython extensions to load.
 #  Default: []
